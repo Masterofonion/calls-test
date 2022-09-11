9c501e6fa42e4a33e381f3932b54564fc15e8f78
Masterofonion <maltsevslava@inbox.ru>
2022-09-11 16:39:57 +0300

    init: app structure

---
+++ src/app/modules/calls/calls.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CallsPageComponent } from './components/calls-page/calls-page.component';
+
+@NgModule({
+  declarations: [CallsPageComponent],
+  imports: [CommonModule],
+})
+export class CallsModule {}
---
+++ src/app/modules/history/history.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HistoryPageComponent } from './components/history-page/history-page.component';
+
+@NgModule({
+  declarations: [HistoryPageComponent],
+  imports: [CommonModule],
+})
+export class HistoryModule {}
